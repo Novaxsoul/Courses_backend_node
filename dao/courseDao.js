91bba5bf61eb2da6c02cb0f63405e667b757c1e5
Juan Cortés <jecortes27@misena.edu.com>
2020-01-23 11:01:52 -0500

    Create a pagination and change an attribute

--- dao/courseDao.js
+++ dao/courseDao.js
@@ -4,15 +4,24 @@ const op = Sequelize.Op;
 
 class CourseDAO {
     findAllCourses(req, res) {
+        let offset = parseInt(req.query.offset)
+        let limit = parseInt(req.query.limit)
+        if(!offset) {offset = 0}
+        if(!limit) {limit = 6}
+        let previous = null
+        if(offset !== 0){
+            previous = `courses/?limit=${limit}&offset=${offset - limit}`
+        }
+        let next = `courses/?limit=${limit}&offset=${offset + limit}`
         if (req.query.search) {
           let search = req.query.search
-          models.Course.findAll({ where: {title: { [op.like]: '%'+ search +'%' } }, raw: true}).then(courses => {
-              res.json({courses});
+          models.Course.findAll({ offset: offset,limit: limit, where: {name: { [op.like]: '%'+ search +'%' } }, raw: true}).then(courses => {
+            res.json({previous,next,offset,limit,courses});
           })
           .catch(err => console.log(err));
         } else {
-          models.Course.findAll().then(courses => {
-              res.json({courses});
+          models.Course.findAll({offset: offset,limit: limit}).then(courses => {
+            res.json({previous,next,offset,limit,courses});
           })
           .catch(err => console.log(err));
         }
@@ -35,7 +44,7 @@ class CourseDAO {
         let data = req.body
         models.Course.findByPk(pk).then(course => {
             course.name = data.name
-            course.descrp = data.descrp
+            course.provider = data.provider
             course.price = data.price
             course.rate = data.rate
             course.credits = data.credits
