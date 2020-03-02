58789b1cf71e82a4b3815a4ee97b442aaace7167
Juan Cortés <jecortes27@misena.edu.com>
2020-03-01 19:37:34 -0500

    Add auth filter for the courses routes

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,13 +1,14 @@
 // Dependencies and files required
 const express = require('express')
 const {getCourses, saveCourse, editCourse, deleteCourse} = require('../controllers/courseController')
-const validator = require('../validator')
+const validator = require('../validator/index')
+const auth = require('../validator/auth')
 const router = express.Router()
 
 // Build the routes
-router.get('/', getCourses)
-router.post('/save', validator.courseValidator, saveCourse)
-router.put('/edit', validator.courseValidator, editCourse)
-router.delete('/delete', deleteCourse)
+router.get('/', auth.jwtfilter, getCourses)
+router.post('/save', [validator.courseValidator, auth.jwtfilter], saveCourse)
+router.put('/edit', [validator.courseValidator, auth.jwtfilter], editCourse)
+router.delete('/delete', auth.jwtfilter, deleteCourse)
 
 module.exports = router
