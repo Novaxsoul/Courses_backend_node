84f59ba21712b07161330d68ac68e4196f19c43a
Juan Cortés <jecortes27@misena.edu.com>
2020-03-01 19:38:34 -0500

    Change a decrepated method and create validator for the user register and login

--- validator/index.js
+++ validator/index.js
@@ -3,13 +3,47 @@ const { check, validationResult } = require('express-validator');
 
 // Export validations for request data
 exports.courseValidator = (req, res, next) => {
-    req.check('name', 'Write a name').notEmpty();
-    req.check('provider', 'Write a provider').notEmpty();
-    req.check('price', 'Write a price').notEmpty();
-    req.check('rate', 'Pick a rate').notEmpty();
-    req.check('credits', 'Pick credits').notEmpty();
-    req.check('image', 'Choose an image').notEmpty();
+    req.check('name', 'Write a name').exists();
+    req.check('provider', 'Write a provider').exists();
+    req.check('price', 'Write a price').exists();
+    req.check('rate', 'Pick a rate').exists();
+    req.check('credits', 'Pick credits').exists();
+    req.check('image', 'Choose an image').exists();
     // Check errors
+    const errors = req.validationErrors()
+
+    if (errors) {
+        const firstError = errors.map((error) => error.msg)[0];
+        return res.status(400).json({error: firstError});
+    }
+
+
+    next()
+}
+
+exports.userValidator = (req, res, next) => {
+    req.check('username', 'Write a username').exists();
+    req.check('password', 'Write a password').exists();
+    req.check('confirm_password', 'Password and password confirmation dont match')
+    .exists()
+    .custom((value) => value === req.body.password)
+
+    const errors = req.validationErrors()
+
+    if (errors) {
+        const firstError = errors.map((error) => error.msg)[0];
+        return res.status(400).json({error: firstError});
+    }
+
+
+    next()
+}
+
+exports.loginValidator = (req, res, next) => {
+
+    req.check('username', 'Write a username').exists();
+    req.check('password', 'Write a password').exists();
+    
     const errors = req.validationErrors()
 
     if (errors) {
